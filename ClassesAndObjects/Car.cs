a7adc885dbddb555b7ad44d7460a7f811408826f
marco-bot-18 <marco.elloyd.consolacion18@gmail.com>
2022-09-11 18:05:02 +0800

    first commit

---
+++ ClassesAndObjects/Car.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//makes properties (class) public and make fields (variables) in private to protect your code - encapsulation
+
+namespace ClassesAndObjects
+{
+    class Car
+    {
+        //public string brand;
+        private int price;
+        private int horsePower;
+
+        public string Brand
+        {   
+           get; set; //automatic property
+        }
+
+        public int Price // <- this is the property named "Price"
+        {
+            get { return price; }
+            set 
+            {
+                if(value <= 0) //value, which represents the value we assign to the property
+                {
+                    price = 0;
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public int HorsePower
+        {
+            set 
+            {
+                if (value <= 0) //value, which represents the value we assign to the property
+                {
+                    horsePower = 0;
+                }
+                else
+                {
+                    horsePower = value;
+                }
+            }
+            get { return horsePower; }
+        }
+
+        public int Speed
+        {
+            set; get;
+        }
+
+        public int DecreaseSpeed(int brake)
+        {
+            Speed = Speed - brake;
+            return Speed;
+        }
+
+
+    }
+
+    class Motor
+    {
+        private int horsePowerMotor; //cannot access in other class
+        private string type;
+        
+        private int Speed;        
+
+        public int HorsePowerMotor
+        {
+            //get { return horsePowerMotor; }
+            //set { horsePowerMotor = value; }
+            set; get;
+        }
+
+        public string Type
+        {
+            set { type = value; }
+            get { return type; }
+        }
+
+    }
+}
---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+
+    class Program
+    {
+        private string X;
+        private string Y;
+        public Program (string y, string x) // <-------- Constructor
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public Program(string y) : this(y, "Unknown") // <-- Overloading Constructor
+        {
+            Console.WriteLine("overloaded constructor.");
+        }
+
+        static void Main(string[] args)
+        {
+            Car();
+            Console.WriteLine("---------------------------------");
+            Motor();
+            Console.WriteLine("---------------------------------");
+
+            Program objProgram = new Program("Sample String", "asd");
+            Console.WriteLine(objProgram.X + " " + objProgram.Y);
+
+            Program objProgram1 = new Program("");
+            Console.WriteLine(objProgram1.X + " " + objProgram1.Y);
+
+        }
+
+        static void Car()
+        {
+            Car car = new Car();
+            car.Brand = "Volvo"; //"class Brand" in Car.cs and so on.....
+            car.Price = -0;
+            car.HorsePower = 1000;
+            car.Speed = 80;
+            Console.WriteLine($"Brand: {car.Brand} \nPrice: {car.Price} \nHorsepower: {car.HorsePower} \nInitial Speed: {car.Speed} \nDecreased Speed in Break: {car.DecreaseSpeed(2)}");
+        }
+
+        static void Motor()
+        {
+            Motor motor = new Motor();
+            motor.HorsePowerMotor = 1000;
+            motor.Type = "BigBike";
+            Console.WriteLine($"{motor.Type} \n{motor.HorsePowerMotor} cc");
+        }
+
+    }
+}
